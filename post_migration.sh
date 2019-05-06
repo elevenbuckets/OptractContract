bf0df0951b7d3521047042444ffcc905d3675b50
KaiLung Sun <kaisun@elevenbuckets.com>
2019-05-06 17:55:55 +0800

    deployed abi and artifact

--- post_migration.sh
+++ post_migration.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-cp build/contracts/BlockRegistry.json build/contracts/QOT.json build/contracts/MemberShip.json build/contracts/Elemmire.json build/contracts/Erebor.json ./dapps/Optract/ABI && \
-     cd dapps/Optract/ABI && \
+TARGET='./dapps/OptractMedia/ABI'
+cp build/contracts/BlockRegistry.json build/contracts/QOT.json build/contracts/MemberShip.json build/contracts/Elemmire.json build/contracts/Erebor.json $TARGET && \
+     cd $TARGET && \
      jq -r '.abi' BlockRegistry.json > BlockRegistry.abi && \
      jq -r '.abi' QOT.json > QOT.abi && \
      jq -r '.abi' MemberShip.json > MemberShip.abi
      jq -r '.abi' Elemmire.json > Elemmire.abi && \
      jq -r '.abi' Erebor.json > Erebor.abi && \
 
-echo $?
-echo "# copied artifact and abi to dapps directory"
+if [ "$?"=="0" ]; then
+    echo "# copied artifact and abi to dapps directory"
+else
+    echo "# Error while copy file to dapps or parse .abi"
+fi
