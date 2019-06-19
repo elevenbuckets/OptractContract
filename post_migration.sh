531b13e59828d000628df122f31a978a0971bf8a
KaiLung Sun <kaisun@elevenbuckets.com>
2019-06-19 17:17:45 +0800

    update old scripts

--- post_migration.sh
+++ post_migration.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
 TARGET='./dapps/OptractMedia/ABI'
-cp build/contracts/BlockRegistry.json build/contracts/QOT.json build/contracts/MemberShip.json build/contracts/Elemmire.json build/contracts/Erebor.json $TARGET && \
+cp build/contracts/BlockRegistry.json build/contracts/QOT.json build/contracts/MemberShip.json $TARGET && \
      cd $TARGET && \
      jq -r '.abi' BlockRegistry.json > BlockRegistry.abi && \
      jq -r '.abi' QOT.json > QOT.abi && \
      jq -r '.abi' MemberShip.json > MemberShip.abi
-     jq -r '.abi' Elemmire.json > Elemmire.abi && \
-     jq -r '.abi' Erebor.json > Erebor.abi && \
 
 if [ "$?"=="0" ]; then
     echo "# copied artifact and abi to dapps directory"
