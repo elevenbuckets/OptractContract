11e4446a6ad1fe1c42ad1cf5585e123de8e16c54
KaiLung Sun <kaisun@elevenbuckets.com>
2019-07-11 12:22:13 +0800

    fix migration bug

--- migrations/2_deploy_Optract.js
+++ migrations/2_deploy_Optract.js
@@ -11,7 +11,7 @@ module.exports = function(deployer) {
     deployer.deploy(QOT).then( () => {
         return deployer.deploy(MemberShip, QOT.address).then ( (iMemberShip) => {
             return deployer.deploy(BlockRegistry, MemberShip.address).then( ()=>{
-                iMemberShip.addAppWhitelist(BlockRegistry.address);
+                return iMemberShip.addAppWhitelist(BlockRegistry.address, 3);  // 0, 1, 2 are for coreManagers
             })
         })
     })
