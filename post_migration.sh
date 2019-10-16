6f2225621584bfb70a324a15e3c78ec66d024a42
KaiLung Sun <kaisun@elevenbuckets.com>
2019-10-16 20:24:15 +0800

    deployed previous commit 5ed3232f8

--- post_migration.sh
+++ post_migration.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 TARGET='./dapps/OptractMedia/ABI'
-cp build/contracts/BlockRegistry.json build/contracts/QOT.json build/contracts/MemberShip.json $TARGET && \
+cp -v build/contracts/BlockRegistry.json build/contracts/QOT.json build/contracts/MemberShip.json build/contracts/flag.json $TARGET && \
      cd $TARGET && \
      jq -r '.abi' BlockRegistry.json > BlockRegistry.abi && \
      jq -r '.abi' QOT.json > QOT.abi && \
-     jq -r '.abi' MemberShip.json > MemberShip.abi
+     jq -r '.abi' MemberShip.json > MemberShip.abi && \
+     jq -r '.abi' flag.json > flag.abi
 
 if [ "$?"=="0" ]; then
     echo "# copied artifact and abi to dapps directory"
